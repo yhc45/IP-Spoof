32cb4e0677954e3f761aa641003890b84f36023f
yhc45 <22433581+yhc45@users.noreply.github.com>
2019-07-02 14:09:28 -0700

    added back port 1

--- scan.py
+++ scan.py
@@ -2,8 +2,7 @@
 from listen import listen_port
 from spoof_struct import send_packet
 
-#removed port 1
-common_ports = [5,7,18,20,21,22,233,25,29,37,42,43,49,53,69,70,79,80,103,\
+common_ports = [1,5,7,18,20,21,22,233,25,29,37,42,43,49,53,69,70,79,80,103,\
              108,109,110,115,118,119,137,139,143,150,156,161,179,190,194,197,\
              389,396,443,444,445,458,546,547,563,569,1080] 
 src_ip = '192.168.100.135'
@@ -15,7 +14,6 @@ def parse_file(file_name):
   f.close()
   black_list = [x.rstrip('\n') for x in content if x[0]!='#']
   return black_list
-#TODO set alert to recv timeout
 
 def main():
   list_b = parse_file("black_list.txt")
