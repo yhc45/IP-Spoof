a4e56acdcfaefc14c8ba7a80157b81a2b1e9c9a5
yhc45 <22433581+yhc45@users.noreply.github.com>
2019-07-02 14:09:32 -0700

    add pickle version

---
+++ pick_measure_ipid.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+from spoof_struct import send_packet
+import dpkt, socket, subprocess
+from collections import defaultdict
+import time
+import cPickle as pickle
+
+src_ip = '192.168.100.128'
+spoof_ip = ''
+src_port = 54024
+pcap_name = "filter.pcap"
+ipid_map=defaultdict(lambda:[])
+
+
+def parse_pcap(file_n):
+  f = open(file_n)
+  pcap = dpkt.pcap.Reader(f)
+
+  for ts, buf in pcap:
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
+    tcp = ip.data
+    #src_addr = socket.inet_ntoa(ip.src)
+    if eth.type == dpkt.ethernet.ETH_TYPE_IP and tcp.dport == src_port: # and tcp.sport == port
+      ipid_map[socket.inet_ntoa(ip.src)].append(ip.id)
+  f.close()
+  return
+
+def parse_candidate(file_name):
+  f = open(file_name,'rb')
+  db = pickle.load(f)
+  return db
+
+def main():
+  parse_ip = parse_candidate("ip_port_record.pickle")
+  #ip = subprocess.Popen(['tcpdump', '-s','0', '-i', 'ens33',
+       '-w', pcap_name, "port", str(src_port)], stdout=subprocess.PIPE)
+  time.sleep(2)
+  for i in range(30):
+    for ip, port in parse_ip.items():
+      #send_packet(src_ip,src_port,ip,port,1,1)
+      #send_packet(spoof_ip,src_port,ip,port,1,1)
+      print("ip: "+ip+" id: "+str(port)+"\n")
+    exit(1)
+    time.sleep(1)
+
+  #p.send_signal(subprocess.signal.SIGTERM)
+  #time.sleep(1)
+  #parse_pcap(pcap_name)
+  #f = open("result_measure.txt","w+")
+  #for ip, id_list in ipid_map.iteritems():
+  #  f.write("ip: "+ip+" id: "+str(id_list)+"\n")
+  #f.close()
+
+
+if __name__ == "__main__":
+  main()
