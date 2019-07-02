9cc970c71ecce14d2d6a4fd95bfe07a36729a6be
yhc45 <22433581+yhc45@users.noreply.github.com>
2019-07-02 14:09:29 -0700

    tcpdump added

--- scan.py
+++ scan.py
@@ -1,14 +1,32 @@
 #!/usr/bin/python
 from listen import listen_port
 from spoof_struct import send_packet
+import dpkt, socket, subprocess
+import time
 
 common_ports = [1,5,7,18,20,21,22,233,25,29,37,42,43,49,53,69,70,79,80,103,\
              108,109,110,115,118,119,137,139,143,150,156,161,179,190,194,197,\
              389,396,443,444,445,458,546,547,563,569,1080] 
-src_ip = '192.168.100.135'
+src_ip = '192.168.100.128'
 src_port = 54002
+pcap_name = "capture.pcap"
 
-def parse_file(file_name):
+
+def parse_pcap(file_n,port):
+  f = open(file_n)
+  pcap = dpkt.pcap.Reader(f)
+  result = []
+
+  for ts, buf in pcap:
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
+    tcp = ip.data
+    if eth.type == dpkt.ethernet.ETH_TYPE_IP and tcp.dport == port:
+      result.append(socket.inet_ntoa(ip.dst))
+  f.close()
+  return result
+
+def parse_blacklist(file_name):
   f = open(file_name,"r")
   content = f.readlines()
   f.close()
@@ -16,23 +34,31 @@ def parse_file(file_name):
   return black_list
 
 def main():
-  list_b = parse_file("black_list.txt")
-  result = set([])
-  for ip in list_b:
-    for port in common_ports:
-      send_packet(src_ip,src_port,ip,port,1,1)
-      if(listen_port(src_ip,src_port)):
-        try:
-          result.remove((ip,"none"))
-        except:
-          pass
-        result.add((ip,port)) 
-        break
-      else:
-        result.add((ip,"none"))
-  f = open("result.txt","w+")
-  for pair in result:
-    f.write(str(pair)+"\n")
+  rem_ip = parse_blacklist("black_list.txt")
+  result = []
+  ports = iter(common_ports)
+  while rem_ip:
+    curr_port = next(ports,None)
+    if curr_port != None:
+      p = subprocess.Popen(['tcpdump', '-s','0', '-i', 'ens33',
+                  '-w', pcap_name, "port", str(src_port)], stdout=subprocess.PIPE)
+      time.sleep(2)
+      for ip in rem_ip:
+        send_packet(src_ip,src_port,ip,curr_port,1,1)
+      #TODO parse cpk package and remove ip from list_b
+      time.sleep(2)
+      p.send_signal(subprocess.signal.SIGTERM)
+      time.sleep(2)
+      ip_res = parse_pcap(pcap_name,curr_port)
+      print(ip_res)
+      rem_ip = [x for x in rem_ip if x not in ip_res]
+      result_temp = [(x,curr_port) for x in ip_res]
+      result += result_temp
+    else: 
+      result_temp = [(x,None) for x in rem_ip]
+  f = open("result_prob.txt","w+")
+  for ip, port in result:
+    f.write(str(ip)+":"+str(port)+"\n")
   f.close()
 
 if __name__ == "__main__":
