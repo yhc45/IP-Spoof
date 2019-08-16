488cb6a2716c423f18430f9fe7dc183ddb377d4e
Michael Chen <22433581+yhc45@users.noreply.github.com>
2019-08-16 16:12:56 -0700

    added proper sleeping time

--- measurement.py
+++ measurement.py
@@ -1,74 +1,84 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 from spoof_struct import send_packet
 import dpkt, socket, subprocess
+from collections import defaultdict
 import time
+import cPickle as pickle
+import itertools
 
-common_ports = [1,5,7,18,20,21,22,233,25,29,37,42,43,49,53,69,70,79,80,103,\
-             108,109,110,115,118,119,137,139,143,150,156,161,179,190,194,197,\
-             389,396,443,444,445,458,546,547,563,569,1080]
 src_ip = '192.168.100.128'
+spoof_ip = '192.168.22.21'
 src_port = 54024
-pcap_name = "capture.pcap"
+pcap_name = "filter.pcap"
+ipid_map=defaultdict(lambda:[])
 
 
-def parse_pcap(file_n,port):
+def parse_pcap(file_n):
   f = open(file_n)
   pcap = dpkt.pcap.Reader(f)
-  result = []
 
   for ts, buf in pcap:
     eth = dpkt.ethernet.Ethernet(buf)
     ip = eth.data
     tcp = ip.data
-    if eth.type == dpkt.ethernet.ETH_TYPE_IP and tcp.dport == src_port and tcp.sport == port:
-      result.append(socket.inet_ntoa(ip.src))
-    elif socket.inet_ntoa(ip.src) != src_ip:
-      print("*************potential abnormal behavior*********")
-      print(socket.inet_ntoa(ip.src))
-      print(tcp.sport)
+    #src_addr = socket.inet_ntoa(ip.src)
+    if eth.type == dpkt.ethernet.ETH_TYPE_IP and tcp.dport == src_port: # and tcp.sport == port
+      ipid_map[socket.inet_ntoa(ip.src)].append(ip.id)
   f.close()
-  return result
+  return
 
-def parse_blacklist(file_name):
-  f = open(file_name,"r")
-  content = f.readlines()
-  f.close()
-  black_list = [x.rstrip('\n') for x in content if x[0]!='#']
-  return black_list
+def parse_candidate(file_name):
+  f = open(file_name,'rb')
+  db = pickle.load(f)
+  return db
 
 def main():
-  #generate the 1000 reflector, given in the result list, has port
-  blacklist_ip = parse_blacklist("black_list.txt")
-  result = []
-  #genertate black list 20:
-  #ports = iter(common_ports)
-  #ports are included in the ip list
+  #parse_ip = parse_candidate("ip_port_record.pickle")
+  #ip = subprocess.Popen(['tcpdump', '-s','0', '-i', 'ens33',
+  #     '-w', pcap_name, "port", str(src_port)], stdout=subprocess.PIPE)
+  #time.sleep(2)
+  beg_ind = 0 
+  end_ind = 6300
+  start = 0
+  #for p in range (131):
+  timestamps = []
+  for i in range(30):
+    timestamps.append(time.time())
+    send_packet(spoof_ip,src_port,"1.1.1.1",500,1,1)
+    
+    #send_packet(spoof_ip,src_port,"1.1.1.1",500,1,1)
+    #send_packet(spoof_ip,src_port,"1.1.1.1",500,1,1)
+    #send_packet(spoof_ip,src_port,"1.1.1.1",500,1,1)
+    #send_packet(spoof_ip,src_port,"1.1.1.1",500,1,1)
+    #send_packet(spoof_ip,src_port,"1.1.1.1",500,1,1)
+    #time.sleep(0.9998061)
+    time.sleep(0.999925)
+    timestamps.append(time.time())
 
-  while rem_ip:
-    curr_port = next(ports,None)
-    if curr_port != None:
-      p = subprocess.Popen(['tcpdump', '-s','0', '-i', 'ens33',
-                  '-w', pcap_name, "port", str(src_port)], stdout=subprocess.PIPE)
-      time.sleep(2)
-      for ip in rem_ip:
-        send_packet(src_ip,src_port,ip,curr_port,1,1)
-      #TODO parse cpk package and remove ip from list_b
-      time.sleep(1)
-      p.send_signal(subprocess.signal.SIGTERM)
-      time.sleep(1)
-      ip_res = parse_pcap(pcap_name,curr_port)
-      print(ip_res)
-      rem_ip = [x for x in rem_ip if x not in ip_res]
-      result_temp = [(x,curr_port) for x in ip_res]
-      result += result_temp
-    else:
-      result_temp = [(x,None) for x in rem_ip]
-      result += result_temp
-      rem_ip=[]
-  f = open("result_prob.txt","w+")
-  for ip, port in result:
-    f.write(str(ip)+":"+str(port)+"\n")
-  f.close()
+    print(timestamps[1]-timestamps[0])
+    return
+    if p == 130:
+      time.sleep(0.136)
+  print(str(sum(result)/29))
+  beg_ind = end_ind
+  end_ind += 6300
+
+  #for i in range(30):
+  #  for ip, port in parse_ip.items():
+      #send_packet(src_ip,src_port,ip,port,1,1)
+  #    send_packet(spoof_ip,src_port,ip,port,1,1)
+  #    print("ip: "+ip+" id: "+str(port)+"\n")
+  #  exit(1)
+  #  time.sleep(1)
+
+  #p.send_signal(subprocess.signal.SIGTERM)
+  #time.sleep(1)
+  #parse_pcap(pcap_name)
+  #f = open("result_measure.txt","w+")
+  #for ip, id_list in ipid_map.iteritems():
+  #  f.write("ip: "+ip+" id: "+str(id_list)+"\n")
+  #f.close()
+
 
 if __name__ == "__main__":
   main()
